8c397210678202098a3a11acdf4d52b97656ab64
Daniel Kelley <dan@xmog.com>
2013-10-06 21:34:32 -0400

    some final changessome final changes

--- client/js/beermenu.js
+++ client/js/beermenu.js
@@ -1,107 +1,72 @@
-function BeerMenuCtrl($scope, $http) {
-  $http.get('data/beers.json').success(function(data) {
-    $scope.beers = data;
-  });
-
-	$scope.predicate = '-name';
-
-	var index = 0;
-	var numberOfColumnms = 3;
-	var numberOfRows = 4;
-
-	function loadBeers(){
-		var beers = [];
-		for (var row=0;row<numberOfRows;row++){
-			for (var col=0;col< numberOfColumnms;col++){
-				index++;
-				console.log("loading beers", index);
-				if (index >= $scope.beers.onTap.length)
-					index = 0;
-				beers.push($scope.beers.onTap[index]);
-			}
-		}
-		console.log("loaded beers");
-		return beers;
-	};
-
-	function displayBeers(beers){		
-		$scope.beerRows = [];
-		var beers = loadBeers();
-		var beerRow = [];
-	    angular.forEach(beers, function(beer) {
-	    	beerRow.push(beer);
-			if(beerRow.length == numberOfColumnms) {
-				if ($scope.beerRows.length < numberOfRows){
-                	$scope.beerRows.push(beerRow);
-                	beerRow = [];
-                }
-            }
-        });
-        if (beerRow.length > 0 && $scope.beerRows.length < numberOfRows)
-        	$scope.beerRows.push(beerRow);
-    }    
-
-	var init = function () {
-		displayBeers();
-        // basic setup
-        $(".beerTable thead th").attr({"colspan" : numberOfColumnms-1});
-        $("td.bottomSpacer").attr({"colspan" : numberOfColumnms});
-        $("td.bottomSpacer").attr({"height" : 100});
-	};
-
-	// factory('time', function($timeout) {
-	//     (function refreshScreen() {
-	//     	// $scope.$apply();
-	//      //  time.now = new Date().toString();
-	//       $timeout(refreshScreen, 1000);
-	//       // How will I write this using setInterval() ?
-	//     })();
-	//   });
-
-	init();
-
-		// rotate through beers
-	//setInterval(, 10000);	
-
-}
 var beerMenuApp = angular.module('beerMenuApp',['beerMenuFilters']);
 
-beerMenuApp.factory('timerReload', function($timeout) {
-    (function refreshScreen() {
-	  	displayBeers();
-		$timeout(displayBeers, 2000);
-      })();
-  });
+var index = 0;
+var rowIndex = 0;
+
+function loadBeers(beerList, numberOfRows, numberOfColumnms){
+	var beers = [];
+	for (var row=0;row<numberOfRows;row++){
+		for (var col=0;col< numberOfColumnms;col++){
+			index++;
+			console.log("loading beers", index);
+			if (index >= beerList.length)
+				index = 0;
+			beers.push(beerList[index]);
+		}
+	}
+	console.log("loaded beers", new Date());
+	return beers;
+};
+
+function displayBeers(beers, numberOfRows, numberOfColumnms){		
+	console.log(beers.length, index, numberOfRows, numberOfColumnms);
+	beerRows = [];
+	var beers = loadBeers(beers, numberOfRows, numberOfColumnms);
+	var beerRow = [];
+    angular.forEach(beers, function(beer) {
+    	beerRow.push(beer);
+		if(beerRow.length == numberOfColumnms) {
+			if (beerRows.length < numberOfRows){
+				console.log("adding", beer.name);
+            	beerRows.push(beerRow);
+            	beerRow = [];
+            }
+        }
+    });
+    if (beerRow.length > 0 && beerRows.length < numberOfRows)
+    	beerRows.push(beerRow);
+    return beerRows;
+}   
+
+function BeerMenuCtrl($scope, $http, $timeout) {
+	$scope.predicate = '-name';
+	
+	$scope.numberOfColumnms = 3;
+	$scope.numberOfRows = 4;
+
+	var init = function () {
+        // basic setup
+        $(".beerTable thead th").attr({"colspan" : $scope.numberOfColumnms-1});
+        $("td.bottomSpacer").attr({"colspan" : $scope.numberOfColumnms});
+        $("td.bottomSpacer").attr({"height" : 100});        
+	};
+
+	$http.get('data/beers.json').success(function(data) {
+		$scope.beers = data.onTap;
+		$scope.beerRows = displayBeers($scope.beers, $scope.numberOfRows, $scope.numberOfColumnms);
+		setInterval(function(){
+        	$scope.$apply(function() {
+            	$scope.beerRows = displayBeers($scope.beers, $scope.numberOfRows, $scope.numberOfColumnms);
+        	});
+    	}, 6000);		
+	});
+
+	init();
+}
 
 beerMenuApp.controller('BeerMenuCtrl', BeerMenuCtrl);
 
-
-//angular.module('timerModule', []).
-  // Declare new object call time,
-  // which will be available for injection
-  
-
-
 $(function() {
-	var div = $("body");
-	// setInterval(function(){
-	//     var pos = div.scrollTop();
-	//     div.animate({ scrollTop: pos + 100 });
-	// }, 3000);
-	// function getDocHeight() {
-	//     var D = document;
-	//     return Math.max(
-	//         Math.max(D.body.scrollHeight, D.documentElement.scrollHeight),
-	//         Math.max(D.body.offsetHeight, D.documentElement.offsetHeight),
-	//         Math.max(D.body.clientHeight, D.documentElement.clientHeight)
-	//     );
-	// }
-	// $(window).scroll(function() {
- //       if($(window).scrollTop() + $(window).height() == getDocHeight()) {
- //          $("html, body").animate({ scrollTop: 0 }, "slow");
- //       }
- //   });
-
 	// 1 Hour = 3600000 Milliseconds
 	var t = setTimeout(function(){location.reload()},3600000);
 	if (console)
